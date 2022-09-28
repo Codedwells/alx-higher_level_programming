bf3b334db2a18cbd92a3600858b5820498675efc
coddedwells <wells.mail.13@gmail.com>
2022-09-28 14:42:59 +0300

    multiple returns removed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, new_matrix[i]))
-    return new_matrix, matrix
+    return new_matrix
